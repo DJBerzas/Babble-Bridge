1be203ec07ebac10f36c4ba67bb31d6a94fe10cc
DJBerzas <djberzas@icloud.com>
2025-03-28 00:21:31 -0400

    handeled

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,13 @@
-import { Stack, Tabs } from "expo-router";
+
+import { Stack } from "expo-router";
 import './global.css';
 
 export default function RootLayout() {
   return (
     <Stack
       screenOptions={{
-        headerShown: false, // 👈 hides all headers
+        headerShown: false, 
       }}
-    >
-      <Stack.Screen name="Tabs" options={{ headerShown: false }} />
-      <Stack.Screen name="Chat_room" options={{ headerShown: false }} />
-      <Stack.Screen name="join_chat" options={{ headerShown: false }} />
-    </Stack>
+    />
   );
 }
