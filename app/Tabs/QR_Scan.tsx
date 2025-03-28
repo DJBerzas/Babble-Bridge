e330ac4bf9a0e148a2da1aee5f808ea3d573390f
wyattb <wbrambl1@students.kennesaw.edu>
2025-03-28 08:12:36 -0400

    QR scanner

--- app/Tabs/QR_Scan.tsx
+++ app/Tabs/QR_Scan.tsx
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Camera, CameraView } from "expo-camera";
+import { Stack } from "expo-router";
+import {
+  AppState,
+  Linking,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from "react-native";
+import { Overlay } from "./Overlay";
+import { useEffect, useRef } from "react";
 
-const QR_Scan = () => {
+export default function QR_Scan() {
+  const qrLock = useRef(false);
+  const appState = useRef(AppState.currentState);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        qrLock.current = false;
+      }
+      appState.current = nextAppState;
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
-    <View>
-      <Text>QR_Scan</Text>
-    </View>
-  )
+    <SafeAreaView style={StyleSheet.absoluteFillObject}>
+      <Stack.Screen
+        options={{
+          title: "Overview",
+          headerShown: false,
+        }}
+      />
+      {Platform.OS === "android" ? <StatusBar hidden /> : null}
+      <CameraView
+        style={StyleSheet.absoluteFillObject}
+        facing="back"
+        onBarcodeScanned={({ data }) => {
+          if (data && !qrLock.current) {
+            qrLock.current = true;
+            setTimeout(async () => {
+              await Linking.openURL(data);
+            }, 500);
+          }
+        }}
+      />
+      <Overlay />
+    </SafeAreaView>
+  );
 }
-
-export default QR_Scan
